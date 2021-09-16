64128e6fcd1eebb0f0fb29b570b7a0d0b910818c
jia1503 <jiagulrajani1503@gamil.com>
2021-09-16 08:37:39 +0530

    changed style

--- config.js
+++ config.js
@@ -1,9 +1,19 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
 
-};
+    const firebaseConfig = {
+      apiKey: "AIzaSyCvbYMDC0AJvWY248wBIMi6IgopfpRm4Gk",
+      authDomain: "project-67-78591.firebaseapp.com",
+      projectId: "project-67-78591",
+      storageBucket: "project-67-78591.appspot.com",
+      messagingSenderId: "1059007667539",
+      appId: "1:1059007667539:web:6fd8b0c3957b3e758b15a6"
+    };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+ 
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
